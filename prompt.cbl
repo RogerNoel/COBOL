d6f0debee50fd0021f3a3e8561be9a828305ccd9
RogerNoel <noel.roger.pro@gmail.com>
2022-09-24 11:21:44 +0200

    Commentaires

--- prompt.cbl
+++ prompt.cbl
@@ -1,11 +1,12 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. interactionUtilisateur.
        AUTHOR. Roger.
-
+      *    Section où on déclare des variables
        DATA DIVISION. 
        WORKING-STORAGE SECTION. 
        01  WS_NOM PIC X(12).
-
+      *    12 caractères sont alloués. Si on ne les utilise pas tous, 
+      *    ils seront remplacés par des espaces.
        PROCEDURE DIVISION.
            DISPLAY 'Interaction utilisateur.'.
            DISPLAY 'Entrez votre ID.'
