f6b67d9c966608207593e18d941882f65f26f109
RogerNoel <noel.roger.pro@gmail.com>
2022-09-24 10:29:18 +0200

    Intercation utilisateur

---
+++ prompt.cbl
@@ -0,0 +1,18 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. intercationUtilisateur.
+       AUTHOR. Roger.
+
+       DATA DIVISION. 
+       WORKING-STORAGE SECTION. 
+       01  WS_NOM PIC X(12).
+
+       PROCEDURE DIVISION.
+           DISPLAY 'Interaction utilisateur.'.
+           DISPLAY 'Entrez votre ID.'
+
+           ACCEPT WS_NOM.
+           
+           DISPLAY 'Bienvenue ' WS_NOM.
+           STOP RUN.
+       
+       
